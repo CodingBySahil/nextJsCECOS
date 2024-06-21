900cae53fa6140efba56442b080f171bde304e0f
codingBySahil <muhammadsahil345737@gmail.com>
2024-06-21 17:38:38 +0500

    tailwind first time in next js

---
+++ 01classtailwind/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-center bg-slate-700 text-white mt-12 flex justify-center align-middle w-full h-full" >
+      <h3 className="bg-amber-400 ">Wellcome to the class</h3>
+    </div>
+  );
+}
