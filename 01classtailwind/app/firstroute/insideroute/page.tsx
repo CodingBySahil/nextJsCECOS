e61b6b5d58641e27d4db52aa08687765e01ebefc
codingBySahil <muhammadsahil345737@gmail.com>
2024-06-21 18:07:28 +0500

    tailwind and routing concept

---
+++ 01classtailwind/app/firstroute/insideroute/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>inside</div>
+  )
+}
+
+export default page
---
+++ 01classtailwind/app/firstroute/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>I am route number one</div>
+  )
+}
+
+export default page
