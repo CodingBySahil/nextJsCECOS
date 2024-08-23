9fd5bfb6437f646f4d0ff04c832243449400231d
codingBySahil <muhammadsahil345737@gmail.com>
2024-08-23 18:30:27 +0500

    05class

--- 04class/src/app/tailwindcss/ch3/page.tsx
+++ 04class/src/app/tailwindcss/ch3/page.tsx
@@ -4,7 +4,10 @@ const Page = () => {
   return (
     <>
       {/*this class comes from globel css I have created using @layer @apply */}
-      <h1 className="mainHeader">This is Tailwind Chapter 3 designs page</h1>
+      {/* <h1 className="mainHeader">This is Tailwind Chapter 3 designs page</h1> */}
+      <h1 className="mt-10 text-center font-bold bg-green-800 text-[8px] xs:text-[16px] xs:bg-slate-800 sm:text-[24px] sm:bg-red-800 sml:text-[32px] sml:bg-red-200 md:text-[40px] md:bg-yellow-300 mdl:text-[48px] mdl:bg-yellow-500 lg:text-[56px] lg:bg-yellow-900 lgl:text-[64px] lgl:bg-teal-600 xl:text-[72px] xl:bg-red-950 xl:text-white">
+        This is heading 1
+      </h1>
     </>
   );
 };
--- 04class/tailwind.config.ts
+++ 04class/tailwind.config.ts
@@ -8,6 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens:{
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
